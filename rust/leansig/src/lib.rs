61ed28f20ee6a11b69833e9f2930ec60d2134500
shaaibu7 <shaaibusuleiman9@gmail.com>
2026-02-15 08:18:04 +0100

    feat: implement xmss key generation

--- rust/leansig/src/lib.rs
+++ rust/leansig/src/lib.rs
@@ -1,6 +1,6 @@
 use leansig::signature::SignatureScheme;
 use leansig::signature::generalized_xmss::instantiations_poseidon_top_level::lifetime_2_to_the_32::hashing_optimized::SIGTopLevelTargetSumLifetime32Dim64Base8;
-
+use rand::{SeedableRng,rngs::StdRng};
 
 pub type LeanSignatureScheme = SIGTopLevelTargetSumLifetime32Dim64Base8;
 pub type LeanPublicKey = <LeanSignatureScheme as SignatureScheme>::PublicKey;
@@ -21,4 +21,41 @@ pub struct Keypair {
     pub secret_key: SecretKey,
 }
 
+impl PublicKey {
+    pub fn new(inner: LeanPublicKey) -> Self {
+        Self { inner }
+    }
+}
 
+impl SecretKey {
+    pub fn new(inner: LeanSecretKey) -> Self {
+        Self { inner }
+    }
+}
+
+/// FFI: Exposed for Go (cgo) interoperability.
+///
+/// # Safety
+/// - `ptr` must be a valid pointer to `len` bytes.
+/// - Caller is responsible for freeing returned memory.
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn leansig_keypair_generate(
+    seed: u64,
+    activation_epoch: usize,
+    num_active_epochs: usize,
+) -> *mut Keypair {
+    let mut rng = StdRng::seed_from_u64(seed);
+    
+    let (pk, sk) = <LeanSignatureScheme as SignatureScheme>::key_gen(&mut rng, activation_epoch, num_active_epochs);
+    
+    let public_key = PublicKey::new(pk);
+    let secret_key = SecretKey::new(sk);
+    
+    let keypair = Box::new(Keypair {
+        public_key,
+        secret_key,
+    });
+    
+    Box::into_raw(keypair)
+}
